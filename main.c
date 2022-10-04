179d3760c0809810618895cde6906d87d5fa8f79
quinton mincy <mincy_q@qwasar.io>
2022-10-04 17:43:38 +0000

    segfault fixed

--- main.c
+++ main.c
@@ -9,3 +9,11 @@ int main(int argc, char** argv){
     }
     return 0;
 }
+//tests:
+    //./my_ls stuff testdir bnuwcbeuri testfile testdir2 main.c -breaks: trys to display unknown file like a directory
+    //./my_ls testdir testdir2 main.c stuff wweeffewrwg testfile -breaks: displays unknown files like regular files
+    //./my_ls stuff niuner hwberffyibewrig testdir testdir2 main.c testfile: works
+
+    //      char* fileName;
+    //      struct stat* sb = malloc(sizeof(struct stat));
+    //      stat(fileName,sb);
