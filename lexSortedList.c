118c75781a7a06ef06bd5c950f91c266017025b4
quinton mincy <mincy_q@qwasar.io>
2022-10-06 23:19:08 +0000

    [UPD]change some architecture stuff :D

--- lexSortedList.c
+++ lexSortedList.c
@@ -1,18 +1,20 @@
 #include "my_ls.h"
+
 entryList* lexSortedList(entryList* enLis){
     entryList* sortedList = malloc(sizeof(entryList));
-    entryList* sortedListCpy = NULL;
+    entryList* head = NULL;
     entryList* nextInLine = NULL;
     entryList* newNode = NULL;
 
     int index = 0;
     int length = countEntries(enLis);
+
     while(index < length){
         nextInLine = lexSort(enLis);
-        newNode = createNodeEnt(nextInLine->sb,nextInLine->path);
+        newNode = createNodeEnt(nextInLine->sb, nextInLine->path);
         if(index == 0){
             sortedList = newNode;
-            sortedListCpy = sortedList;
+            head = sortedList;
         }else{
             sortedList->next = newNode;
             sortedList = sortedList->next;
@@ -22,5 +24,5 @@ entryList* lexSortedList(entryList* enLis){
     }
     sortedList = sortedList->next;
     free(sortedList);
-    return sortedListCpy;
+    return head;
 }
--- timeSort.c
+++ timeSort.c
@@ -1,27 +1,24 @@
 #include "my_ls.h"
-entryList* timeSort(entryList* head){
-    struct stat* statBuff = malloc(sizeof(struct stat));
-    char* path = head->path;
-    stat(path, statBuff);
+entryList* timeSort(entryList* list){
+    entryList* nextInLine = list;
 
-    entryList* nextInLine = head;
-    nextInLine->sb = statBuff;
+    list = list->next;
 
-    head = head->next;
-
-    while(head != NULL){
-        statBuff = malloc(sizeof(struct stat));
-        path = head->path;
-        stat(path, statBuff);
-
-        if(statBuff->st_mtim.tv_sec > nextInLine->sb->st_mtim.tv_sec){
-            nextInLine = head;
-        }else if(statBuff->st_mtim.tv_sec == nextInLine->sb->st_mtim.tv_sec){
-            if(statBuff->st_mtim.tv_nsec > nextInLine->sb->st_mtim.tv_nsec){
-                nextInLine = head;
+    while(list != NULL){
+        if(list->sb->st_mtim.tv_sec > nextInLine->sb->st_mtim.tv_sec){
+            nextInLine = list;
+        }
+        else if (list->sb->st_mtim.tv_sec == nextInLine->sb->st_mtim.tv_sec) {
+            if (list->sb->st_mtim.tv_nsec > nextInLine->sb->st_mtim.tv_nsec) {
+                nextInLine = list;
+            }
+            else if (list->sb->st_mtim.tv_nsec == nextInLine->sb->st_mtim.tv_nsec) {
+                if(my_strcmp(list->path, nextInLine->path) < 0) {
+                    nextInLine = list;
+                }
             }
         }
-        head = head->next;
+        list = list->next;
     }
     return nextInLine; 
 }
